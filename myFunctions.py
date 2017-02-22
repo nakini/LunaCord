01ca9079cb5ebc5497e6b9789421bd5f3e3a708c
nakini <tnb88@mail.missouri.edu>
2017-02-22 13:29:12 -0600

    Few self defined functions

---
+++ myFunctions.py
@@ -0,0 +1,10 @@
+"""
+Here, some of the functions which are not available in math library. I am pretty sure,
+we can find them in other libraries like numpy, pandas, etc.
+"""
+
+import math as mt
+
+
+def subList(a, b):
+    return [b[0]-a[0], b[1]-a[1]]
